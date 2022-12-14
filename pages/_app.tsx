e1eb6c10e3274495ace842b97edb4608bc7957a0
Minsoo Kim <minsooerickim@gmail.com>
2022-12-14 09:50:33 +0900

    add tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import { AppProps } from "next/app";
 
+import 'tailwindcss/tailwind.css'
+import '../styles/globals.css'
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Component {...pageProps} />
