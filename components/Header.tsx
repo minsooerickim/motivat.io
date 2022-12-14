98603d7e78a281bf0a1bfb2e7ff1d1c37bf2b244
Minsoo Kim <minsooerickim@gmail.com>
2022-12-14 15:02:55 +0900

    add header

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="py-4">
+            <h1 className="text-3xl">Motivatio</h1>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,12 @@ import React from "react"
 import SliderComponent from "@/components/SliderComponent"
 import { Page } from "@/components/Page/Page"
 import QuotePlayer from "@/components/QuotePlayer"
+import Header from "@/components/Header"
 
 export default function Home() {
   return (
     <Page title="Home">
+      <Header />
       <QuotePlayer />
       <SliderComponent />
     </Page>
