be6f52e9049cf9d5d7d5a4b1fa51781815e2e9ae
Minsoo Kim <minsooerickim@gmail.com>
2022-12-16 20:41:32 +0900

    add utility functions

--- utility/methods.ts
+++ utility/methods.ts
@@ -6,3 +6,14 @@ export function getRandomArbitrary(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
 }
+
+/**
+ * Validates if string is an email address
+ */
+export function validateEmail(email) {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
