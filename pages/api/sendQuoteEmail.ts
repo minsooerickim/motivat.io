f6568a34ab678944fe5b47931b7041cbfc7c5368
Minsoo Kim <minsooerickim@gmail.com>
2022-12-19 19:35:19 +0900

    add sendQuoteEmail and sendQuotePhone endpoint

---
+++ pages/api/sendQuoteEmail.ts
@@ -0,0 +1,70 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { sendEmail } from '@/lib/sendgrid'
+import { PrismaClient } from '@prisma/client'
+import axios from 'axios'
+
+export default async function manualReviewApplication(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const sgMail = require('@sendgrid/mail')
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  
+  const prisma = new PrismaClient()
+
+  const emailSubscribers = await prisma.subscription.findUnique({
+    where: {
+      subscriptionId: 'email',
+    },
+    include: {
+      users: true,
+    },
+  })
+
+  const quoteObj = await axios.get('http://api.quotable.io/random?')
+  const quoteContent = quoteObj.data.content
+  const quoteAuthor = quoteObj.data.author
+
+  emailSubscribers.users.forEach(async (subscriber) => {
+    if (subscriber['email'] != null) {
+      const msg = {
+        to: subscriber['email'],
+        from: 'minsooerickim@gmail.com', // Use the email address or domain you verified above
+        subject: 'Daily Motivational Quote',
+        text: quoteAuthor,
+        html:
+          '<strong>' + quoteContent + '</strong>' + '<br></br>' + quoteAuthor,
+      }
+      await sgMail.send(msg).then(
+        () => {
+          console.log('successfully sent email to ' + subscriber['email'])
+        },
+        (error: { response: { body: any } }) => {
+          console.error(error)
+          if (error.response) {
+            console.error(error.response.body)
+          }
+        }
+      )
+    }
+  })
+
+  //TODO: pretty email using templates
+  // await sendEmail({
+  //       // email: subscriber['email'],
+  //       email: 'minsooerickim@gmail.com',
+  //       quote: quoteContent,
+  //       author: quoteAuthor,
+  //       template_id: process.env.SENDGRID_APPLICATION_ACCEPTANCE_EMAIL_TEMPLATE_ID,
+  //     })
+
+  // const { email, quote, author } = req.body
+
+  // await sendEmail({
+  //   email: email,
+  //   quote: quote,
+  //   author: author,
+  //   template_id: process.env.SENDGRID_APPLICATION_ACCEPTANCE_EMAIL_TEMPLATE_ID,
+  // })
+  res.status(200).json({})
+}
---
+++ pages/api/sendQuotePhone.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { sendEmail } from '@/lib/sendgrid'
+import { PrismaClient } from '@prisma/client'
+
+export default async function manualReviewApplication(res: NextApiResponse) {
+  const prisma = new PrismaClient()
+
+  const emailSubscribers = await prisma.subscription.findUnique({
+    where: {
+      subscriptionId: 'phone',
+    },
+    include: {
+      users: true,
+    },
+  })
+  console.log(emailSubscribers)
+  // const { email, quote, author } = req.body
+
+  // await sendEmail({
+  //   email: email,
+  //   quote: quote,
+  //   author: author,
+  //   template_id: process.env.SENDGRID_APPLICATION_ACCEPTANCE_EMAIL_TEMPLATE_ID,
+  // })
+  res.status(200).json({})
+}
