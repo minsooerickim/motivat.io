6dd88fed1cb6a2ba0fb489d7b08e767957b697ac
Minsoo Kim <minsooerickim@gmail.com>
2022-12-23 21:25:42 +0900

    add welcome email upon subscription

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -1,15 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client'
 import { validateEmail } from '@/utility/methods'
+import axios from 'axios'
 
 export default async function manualReviewApplication(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const sgMail = require('@sendgrid/mail')
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
   const prisma = new PrismaClient()
   const reqLen = Object.keys(req.body).length
 
   // if both email and phone
+  // TODO: still need to find a phone api provider
   if (reqLen == 2) {
     const { email, phone } = req.body
 
@@ -43,7 +47,29 @@ export default async function manualReviewApplication(
         subscriptions: { connect: { subscriptionId: 'email' } },
       },
     })
-    return res.status(200).json({ user })
+
+    const email = req.body.email
+    const msg = {
+      to: email,
+      from: 'minsooerickim@gmail.com', // Use the email address or domain you verified above
+      subject: 'Welcome to Motivat.io! :)',
+      html:
+        '<strong>' +
+        'Welcome to Motivat.io. Stay motivated with our daily quotes to come!' +
+        '</strong>',
+    }
+    await sgMail.send(msg).then(
+      () => {
+        return res.status(200).json({ user })
+      },
+      (error: { response: { body: any } }) => {
+        console.error(error)
+        if (error.response) {
+          console.error(error.response.body)
+          return res.status(400).json({ user })
+        }
+      }
+    )
   }
 
   // if just phone
