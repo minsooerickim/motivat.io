aea872e3bc2e90d0111392b2f01699094c32749c
Minsoo Kim <minsooerickim@gmail.com>
2022-12-31 16:08:46 +0900

    add get request const

--- utility/const.ts
+++ utility/const.ts
@@ -1,1 +1,7 @@
-export const TITLE = "Motivat.io"
+export const TITLE = 'Motivat.io'
+export const GET_CONFIG = {
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+  },
+}
