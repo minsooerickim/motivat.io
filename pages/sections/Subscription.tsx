65c9e5915bd19c007e3763d41028b08b4d9fa13e
Minsoo Kim <minsooerickim@gmail.com>
2022-12-31 16:10:56 +0900

    update subscription text

--- pages/sections/Subscription.tsx
+++ pages/sections/Subscription.tsx
@@ -1,16 +1,19 @@
-import ColorCheckboxes from '@/components/Checkbox'
+import Checkbox from '@/components/Checkbox'
 
 export default function Subscription() {
-  const HOOK = 'Get Daily Quotes!'
-  const DESCRIPTION = 'we will not send any spam :)'
+  const HOOK = 'Subscribe to Get Daily Quotes!'
+  const DESCRIPTION =
+    'We will not send any spam or sell any of your information :)'
+  const DESCRIPTION2 = 'You can cancel the subscription at any time at no cost!'
 
   return (
-    <div className="flex-col py-4">
+    <div className="flex-col py-4 items-center justify-center">
       <div className="flex flex-col items-center">
         <h1 className="text-lg text-">{HOOK}</h1>
-        <p className=" text-xs text-opacity-70 text-sub">{DESCRIPTION}</p>
+        <p className=" text-xs text-opacity-70 text-sub pt-1">{DESCRIPTION}</p>
+        <p className=" text-xs text-opacity-70 text-sub pt-1">{DESCRIPTION2}</p>
       </div>
-      <ColorCheckboxes />
+      <Checkbox />
     </div>
   )
 }
